e1335bc036906c1086872ba9a5fe81cd505bf113
umaicode <park.seungchan99@gmail.com>
2026-03-02 00:34:42 +0900

    add: 주문 리포지토리 개발

---
+++ jpashop/src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -0,0 +1,26 @@
+package jpabook.jpashop.repository;
+
+import jakarta.persistence.EntityManager;
+import jpabook.jpashop.domain.Order;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class OrderRepository {
+
+    private final EntityManager em;
+
+    public void save(Order order) {
+        em.persist(order);
+    }
+
+    public Order findOne(Long id) {
+        return em.find(Order.class, id);
+    }
+
+    // 이거 나중에 설명. 동적 쿼리여서 어려움.
+//    public List<Order> findAll(OrderSearch orderSearch) {}
+}
